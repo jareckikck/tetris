1054c434b151514fcb6919ac92c3f25504a9cd86
Jarek Grabarczyk <jaroslaw.grabarczyk@projectic.pl>
2019-09-02 20:53:29 +0200

    class update

--- chunk.js
+++ chunk.js
@@ -1,16 +1,15 @@
 class Chunk {
-	constructor(x, y, rowsX, rowsY) {
+	constructor(x, y, shape) {
 		this.x = x || 0;
 		this.y = y || 0;
-		this.rowsX = rowsX || 1;
-		this.rowsY = rowsY || 1;
+		this.shape = shape || 1;
 		this.active = true;
 	}
 	getWidth() {
-		return this.rowsX * CHUNKSIZE;
+		return this.shape * CHUNKSIZE;
 	}
 	getHeight() {
-		return this.rowsY * CHUNKSIZE;
+		return this.shape * CHUNKSIZE;
 	}
 
 	draw(color) {
@@ -30,18 +29,18 @@ class Chunk {
 }
 
 class Piece extends Chunk {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.chunkArr = [];
+	constructor(x, y, shape) {
+		super(x, y, shape);
+		this.shape = shape || [[1, 1], [1, 1]];
 	}
 	moveDown() {
 		this.y = this.y + SPEED;
 	}
 
 	isActive() {
-    return this.getHeight() + this.y < CANVAS_HEIGHT;
-    //  change to method 
-    // colided with floor 
+		return this.getHeight() + this.y < CANVAS_HEIGHT;
+		//  change to method
+		// colided with floor
 	}
 
 	update() {
@@ -56,160 +55,21 @@ class Piece extends Chunk {
 		centerY = (2 * this.y + this.rowsY * CHUNKSIZE) / 2;
 		return { centerX, centerY };
 	}
-}
-
-class PieceShapeI extends Piece {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.rowsX = 1;
-    this.rowsY = 4;
-    this.chunkArr = [];
-	}	
 	draw() {
-		const color = "blue";
-		this.chunkArr = [];
 		let i;
-		for (i = 0; i < 4; i++) {
-			this.chunkArr.push(new Chunk(this.x, this.y + i * CHUNKSIZE));
-		}
-		this.chunkArr.forEach(function(chunk) {
-			chunk.draw(color);			
-		});
-	}
-}
-
-class PieceShapeL extends Piece {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.rowsX = 2;
-		this.rowsY = 3;
-	}
-	draw() {
-		let chunkArr = [];
-		let i;
-		const color = "green";
-		for (i = 0; i < 4; i++) {
-			if (i != 3) {
-				chunkArr.push(new Chunk(this.x, this.y + i * CHUNKSIZE));
-			} else {
-				chunkArr.push(
-					new Chunk(this.x + CHUNKSIZE, this.y + (i - 1) * CHUNKSIZE)
-				);
+		let chunks = [];
+		let pushX, pushY;
+		this.shape.forEach((rowY, indexY) => {
+			for (i = 0; i < rowY.length; i++) {
+				if (rowY[i] > 0) {
+					pushX = this.x + i * CHUNKSIZE;
+					pushY = this.y + indexY * CHUNKSIZE;
+					chunks.push(new Chunk(pushX, pushY));
+				}
 			}
-		}
-		chunkArr.forEach(function(chunk) {
-			chunk.draw(color);
 		});
-	}
-}
-
-class PieceShapeJ extends Piece {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.rowsX = 2;
-		this.rowsY = 3;
-	}
-	draw() {
-		let chunkArr = [];
-		let i;
-		const color = "chartreuse";
-		for (i = 0; i < 4; i++) {
-			if (i != 3) {
-				chunkArr.push(new Chunk(this.x + CHUNKSIZE, this.y + i * CHUNKSIZE));
-			} else {
-				chunkArr.push(new Chunk(this.x, this.y + (i - 1) * CHUNKSIZE));
-			}
-		}
-		chunkArr.forEach(function(chunk) {
-			chunk.draw(color);
-		});
-	}
-}
-
-class PieceShapeZ extends Piece {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.rowsX = 3;
-		this.rowsY = 2;
-	}
-
-	draw() {
-		let chunkArr = [];
-		let i;
-		for (i = 0; i < 2; i++) {
-			chunkArr.push(new Chunk(this.x + i * CHUNKSIZE, this.y));
-			chunkArr.push(
-				new Chunk(this.x + (i + 1) * CHUNKSIZE, this.y + CHUNKSIZE)
-			);
-		}
-		chunkArr.forEach(function(chunk) {
+		chunks.forEach(chunk => {
 			chunk.draw();
-		});
-	}
-}
-
-class PieceShapeS extends Piece {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.rowsX = 3;
-		this.rowsY = 2;
-	}
-
-	draw() {
-		let chunkArr = [];
-		let i;
-		const color = "violet";
-		for (i = 0; i < 2; i++) {
-			chunkArr.push(new Chunk(this.x + (i + 1) * CHUNKSIZE, this.y));
-			chunkArr.push(new Chunk(this.x + i * CHUNKSIZE, this.y + CHUNKSIZE));
-		}
-		chunkArr.forEach(function(chunk) {
-			chunk.draw(color);
-		});
-	}
-}
-
-class PieceShapeT extends Piece {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.rowsX = 3;
-		this.rowsY = 2;
-	}
-
-	draw() {
-		let chunkArr = [];
-		let i;
-		const color = "brown";
-		for (i = 0; i < 4; i++) {
-			if (i != 3) {
-				chunkArr.push(new Chunk(this.x + i * CHUNKSIZE, this.y + CHUNKSIZE));
-			} else {
-				chunkArr.push(new Chunk(this.x + CHUNKSIZE, this.y));
-			}
-		}
-		chunkArr.forEach(function(chunk) {
-			chunk.draw(color);
-		});
-	}
-}
-
-class PieceShapeO extends Piece {
-	constructor(x, y, rowsX, rowsY) {
-		super(x, y, rowsX, rowsY);
-		this.rowsX = 2;
-		this.rowsY = 2;
-	}
-
-	draw() {
-		let chunkArr = [];
-		let i;
-		const color = "yellow";
-		for (i = 0; i < 2; i++) {
-			chunkArr.push(new Chunk(this.x + i * CHUNKSIZE, this.y));
-			chunkArr.push(new Chunk(this.x + i * CHUNKSIZE, this.y + CHUNKSIZE));
-		}
-		chunkArr.forEach(function(chunk) {
-			chunk.draw(color);
 		});
 	}
 }
